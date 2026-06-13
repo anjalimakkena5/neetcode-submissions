fcae57e977176002bb94e81cb3b4330fedc3fc36
anjalimakkena5 <anjalimakkena.252cs018@nitk.edu.in>
2026-06-13 15:57:51 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int func(int ind,vector<int>&dp){
+        if(ind==0 ||ind==1) return 1;
+        
+        if(dp[ind]!=-1) return dp[ind];
+        return dp[ind]=func(ind-1,dp)+func(ind-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return func(n,dp);
+        
+    }
+};
