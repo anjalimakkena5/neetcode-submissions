09d2e0c7fc590ba487d9d64fd93672c8a01009bb
anjalimakkena5 <anjalimakkena.252cs018@nitk.edu.in>
2026-07-13 19:16:12 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.size();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            if(st.empty() || s[i]=='(' || s[i]=='[' ||s[i]=='{'){
+                st.push(s[i]);
+            }
+            else if((s[i]==')' && st.top()=='(') || (s[i]==']' && st.top()=='[') ||(s[i]=='}' && st.top()=='{'))
+            {
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return st.empty();
+    }
+};
