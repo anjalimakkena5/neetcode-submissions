1985800ce6ddc124e8533c7b27c027eb7f7c5f1f
anjalimakkena5 <anjalimakkena.252cs018@nitk.edu.in>
2026-07-23 14:28:20 +0530

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool static myfunc(vector<int>a,vector<int> b){
+        return a[0]<b[0];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),myfunc);
+        vector<vector<int>>ans;
+        int n=intervals.size();
+        for(int i=0;i<n;i++){
+            if(ans.empty() || ans.back()[1]<intervals[i][0]){
+                ans.push_back(intervals[i]);
+            }
+            else{
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            }
+        }
+        return ans;
+    }
+};
